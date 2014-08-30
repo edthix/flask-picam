cffda65aabe2fa32407deee4aa30e3b35fa232f1
Edham Arief / edthix <edthix@gmail.com>
2014-08-31 05:58:32 +0800

    Basic snap function

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
-from time import gmtime, strftime
+from flask import Flask, render_template, redirect, url_for, flash, get_flashed_messages
+import time
+import picamera
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_lol' # for flash
 
 @app.route('/')
 def home():
@@ -11,8 +13,16 @@ def home():
 def snap():
     # take image using pi camera
     # Save to /static/images/image-**timestamp**.jpg
-    filename = "image-" + strftime("%Y%m%d%H%M%S", gmtime()) + ".jpg"
-    #return redirect(url_for('home'))
+    filename = "static/images/image-" + time.strftime("%Y%m%d%H%M%S", time.gmtime()) + ".jpg"
+    with picamera.PiCamera() as camera:
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(filename)
+        camera.stop_preview()
+
+    flash('Image captured in ' + filename)
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
